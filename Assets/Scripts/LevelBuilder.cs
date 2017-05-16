c1ba5ab0ea47e4c02712bbd64da9772aa8d36331
Sandro Machaidze <sandromachiadze33@gmail.com>
2017-05-16 20:50:32 +0400

    some updates

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -4,9 +4,45 @@ using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour {
     public Transform player;
-    public Vector3 lastPLatformPosition;
+    public int numberOfStartingPlatforms;
+   //clone this object in every 50 meteres
+    public Vector3 platformSpawnPoint;
+    public GameObject [] platforms;
+    public float destroyTime = 5f;
+    //Unity custom funcitons
+    void Start()
+    {
+        int counter = 0;
+        while (counter < numberOfStartingPlatforms)
+        {
+            CreateNextPlatform();
+            counter = counter + 1;
+        }
+    }
+    int PP;
+    int lastPP = 0;
 	void Update ()
     {
-        float pp = (int)player.position.z / 50;
-	}
+        PP = (int)(player.position.z / 50);
+        if(PP > lastPP)
+        {
+            CreateNextPlatform();
+        }
+        lastPP = PP;
+    }
+    //my custom functions
+    void CreateNextPlatform()
+    {
+        Debug.Log("Creating Platform");
+
+        platformSpawnPoint.z = platformSpawnPoint.z + 50f;
+
+        int platformChooser = Random.Range(0, platforms.Length);
+
+        GameObject clone;
+        clone = Instantiate(platforms[platformChooser]);
+
+        clone.transform.position = platformSpawnPoint;
+        Destroy(clone, destroyTime);
+    }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,34 +4,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public AudioSource sfxAudioPlayer;
+    public AudioClip jumpUpSound;
+    public AudioClip jumpDownSound;
+    public AudioClip gameOverSound;
+
     public Rigidbody myBody;
+
+    public float rayCastDistance;
     public float maxSpeed;
     public float moveAcceleration;
     public float jumpVelocity;
     public float jumpAcceleration;
     private bool isGrounded = false;
+    private bool isGameOver = false;
+    public float gameOverHeight = -5f;
 
     void FixedUpdate()
     {
+        GroundChecker();
         ConstantMove();
 
     }
    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true )
+        if(transform.position.y < gameOverHeight && isGameOver == false)
+        {
+            isGameOver = true;
+            sfxAudioPlayer.PlayOneShot(gameOverSound);
+        }
+
+        if (isGrounded &&(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0) ) )
+
         {
             jump();
         }
+        
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * rayCastDistance);
     }
     void OnCollisionEnter(Collision collisionInfo)
     {
-
-        isGrounded = true;
+        //isGrounded = true;
+        sfxAudioPlayer.PlayOneShot(jumpDownSound);
     }
+
     void OnCollisionExit(Collision collisionInfo)
     {
-        isGrounded = false;
+        //isGrounded = false;
     }
     //void OnCollisionStay(Collision collisionInfo)
     //{
@@ -60,5 +80,14 @@ public class PlayerMovement : MonoBehaviour
         Vector3 jumpVelocity = myBody.velocity;
         jumpVelocity.y = jumpVelocity.y + jumpAcceleration;
         myBody.velocity = jumpVelocity;
+        sfxAudioPlayer.PlayOneShot(jumpUpSound);
+        
+    }
+    void GroundChecker()
+    {
+        Ray ray = new Ray();
+        ray.origin = transform.position;
+        ray.direction = Vector3.down;
+        isGrounded = Physics.Raycast(ray, rayCastDistance);
     }
 }
